05f62c696120ed4ed5b8e7153ef129c00f76f8a4
masted <user@masted.(none)>
2013-11-13 03:08:34 +0400

    ...

---
+++ GitRepo.class.php
@@ -0,0 +1,18 @@
+<?php
+
+trait GitRepo
+{
+    use RemoteCommands;
+
+    function password($host) {
+        return 'qfg8yf32eqfw';
+    }
+
+    function download()
+    {
+        die2(123);
+        //`ssh `;
+        // EnvInstaller. ф-ии связанные с формированием \удалённых\ комманд, \получающих данные\
+    }
+
+}
