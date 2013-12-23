71b60637efe4139d67a4aed2dcac7d6a4804290c
masted <masted311@gmail.com>
2013-12-23 17:47:49 +0400

    Commit was made from server /localhost by ci/push

--- Ci.class.php
+++ Ci.class.php
@@ -128,22 +128,13 @@ class Ci extends GitBase {
     return $files;
   }
 
-  //function aaa() {
-  //  $currentCron = $this->shellexec("crontab -l");
-  //  die2($currentCron);
-  //}
-
   function updateCron() {
     $cron = '';
     foreach ($this->findCronFiles() as $file) $cron .= trim(file_get_contents($file))."\n";
-    $cron .= $this->shellexec('php /home/user/ngn-env/pm/pm.php localProjects cron');
-    if ($this->server['sType'] != 'prod') $cron .= "15 1 * * * php /home/user/ngn-env/ci/update\n"; // 01:15
+    $cron .= $this->shellexec('php ~/ngn-env/pm/pm.php localProjects cron');
+    if ($this->server['sType'] != 'prod') $cron .= "15 1 * * * php ~/ngn-env/ci/update\n"; // 01:15
     $currentCron = $this->shellexec("crontab -l");
-
-    if (strstr($currentCron, 'no crontab')) {
-      output('no cron');
-      return;
-    }
+    die2([$cron, $currentCron]);
     if ($cron and $cron != $currentCron) {
       file_put_contents('/tmp/.crontab', $cron);
       print $this->shellexec("crontab /tmp/.crontab");
