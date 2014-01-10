e83ab1a4cafbed2574e900024e2d7005beeba60a
masted <masted311@gmail.com>
2014-01-10 13:35:58 +0400

    Commit was made from server localhost by ci/push

--- Git.class.php
+++ Git.class.php
@@ -10,6 +10,9 @@ class Git extends GitBase {
 
   function __call($method, $args) {
     foreach ($this->findGitFolders() as $folder) {
+//
+      if (!Misc::hasSuffix('/ngn', $folder)) continue;
+prr($folder);
       call_user_func_array([new GitFolder($folder), $method], $args);
     }
     chdir($this->cwd);
