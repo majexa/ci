565c41fd3347def502879bd3d1717614464e9468
masted <masted311@gmail.com>
2014-10-21 20:45:35 +0400

    Auto-commit from localhost

--- 1.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-$f = new GitFolder('/home/user/ngn-env/projects/sboards');
-//$f = new GitFolder('/home/user/ngn-env/casper-ngn');
-print `git fetch origin 2>&1`;
-//print '!'.Cli::pipeCmd('git branch -r').'!';
